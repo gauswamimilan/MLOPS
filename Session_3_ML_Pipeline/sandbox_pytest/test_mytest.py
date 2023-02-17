56b2c876cb9e5e4820070b0911ef2ebb25ace0e0
milan <gauswamimilangiri@gmail.com>
2023-02-17 09:47:28 +0530

    adding model api serving

--- Session_3_ML_Pipeline/sandbox_pytest/test_mytest.py
+++ Session_3_ML_Pipeline/sandbox_pytest/test_mytest.py
@@ -1,7 +1,17 @@
 from mytest import square
+import pytest
+
+# a way to create a parameter value as a function argument
+@pytest.fixture
+def my_fixture():
+    return 2
 
 
 def test_square():
     assert square(2) == 4
     assert square(3) == 9
-    
+
+
+def test_square_wih_fixure(my_fixture):
+    assert square(my_fixture) == 4
+    assert square(3) == 9
